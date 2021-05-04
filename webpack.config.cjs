d0b9a7f3f96fb046a04a2e0fb9c6b030e16b86b9
VilerIT <vilerinftech@gmail.com>
2021-05-04 11:41:11 +0400

    move favicon to assets

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -22,7 +22,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: 'index.html',
-      favicon: './assets/images/favicon.png',
+      favicon: './assets/favicon.png',
     }),
   ],
 };
